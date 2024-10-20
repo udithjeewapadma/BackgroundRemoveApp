b5ec9e3130ad2f11b06a381e2abf244e96718e9e
Jeewapadma <udithjeewapadma117@gmail.com>
2024-10-20 18:08:23 +0530

    create pages and setting routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Result from './pages/Result'
+import BuyCredit from './pages/BuyCredit'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/result' element={<Result/>} />
+        <Route path='/buy' element={<BuyCredit/>} />
+      </Routes>
+
+    </div>
   )
 }
 
